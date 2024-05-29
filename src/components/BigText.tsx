c861167f37833cc7d0e5ff6731644ecdc5eabc56
mkuzmick <mkuzmick@gmail.com>
2024-05-29 11:36:24 -0400

    Updates:

--- src/components/BigText.tsx
+++ src/components/BigText.tsx
@@ -1,6 +1,6 @@
+import { ReactNode } from 'react'
 
-
-export default function BigText({children}) {
+export default function BigText({children}: {children: ReactNode;}) {
     return (
       <>
         <h1 className="text-4xl md:text-7xl font-black">{children}</h1>
