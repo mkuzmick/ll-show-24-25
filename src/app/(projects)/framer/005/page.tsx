fcefc352ccd255ef18b9a457fde1e4d61c751702
mkuzmick <mkuzmick@gmail.com>
2024-05-31 20:34:32 -0400

    Updates:

--- src/app/(projects)/framer/005/page.tsx
+++ src/app/(projects)/framer/005/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <CenteredContainer>
       <div style={{ color: 'white', textAlign: 'center' }}>
-        <h1 className='font-black text-9xl mix-blend-exclusion opacity-80 '>ll summer</h1>
+        <h1 className='font-black text-9xl text-yellow-500 mix-blend-hard-light opacity-90 '>ll summer</h1>
         <p>all the things.</p>
       </div>
     </CenteredContainer>
